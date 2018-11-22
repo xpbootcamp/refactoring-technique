0d24ed61cb9df774ba7dfc7f85ffbba004e1a767
Yuan Shenjian <sjyuan@thoughtworks.com>
2018-11-22 20:46:43 +0800

    R8: Replace Conditional with Polymorphism for getCharge
    Signed-off-by: Yuan Shenjian <sjyuan@thoughtworks.com>

--- src/main/java/cc/xpbootcamp/helloworld/NewReleasePrice.java
+++ src/main/java/cc/xpbootcamp/helloworld/NewReleasePrice.java
@@ -5,4 +5,9 @@ public class NewReleasePrice extends Price {
     int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
+
+    @Override
+    public double getCharge(int daysRented) {
+        return daysRented * 3;
+    }
 }
--- src/main/java/cc/xpbootcamp/helloworld/Price.java
+++ src/main/java/cc/xpbootcamp/helloworld/Price.java
@@ -1,31 +1,7 @@
 package cc.xpbootcamp.helloworld;
 
-import static cc.xpbootcamp.helloworld.Movie.CHILDREN;
-import static cc.xpbootcamp.helloworld.Movie.NEW_RELEASE;
-import static cc.xpbootcamp.helloworld.Movie.REGULAR;
-
 public abstract class Price {
     abstract int getPriceCode();
 
-    public double getCharge(int daysRented){
-        double result = 0;
-        switch (getPriceCode()) {
-            case REGULAR:
-                result += 2;
-                if (daysRented > 2) {
-                    result += (daysRented - 2) * 1.5;
-                }
-                break;
-            case NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case CHILDREN:
-                result += 1.5;
-                if (daysRented > 3) {
-                    result += (daysRented - 3) * 1.5;
-                }
-                break;
-        }
-        return result;
-    }
+    public abstract double getCharge(int daysRented);
 }
