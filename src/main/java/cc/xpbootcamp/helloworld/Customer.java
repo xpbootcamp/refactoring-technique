7c4e712242c40b9be0a5457af3abb046dda4f3f5
Yuan Shenjian <sjyuan@thoughtworks.com>
2018-11-22 19:13:08 +0800

    R4: Replace Temp with Query for two temporary variables
    Signed-off-by: Yuan Shenjian <sjyuan@thoughtworks.com>

--- src/main/java/cc/xpbootcamp/helloworld/Customer.java
+++ src/main/java/cc/xpbootcamp/helloworld/Customer.java
@@ -20,23 +20,27 @@ public class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
         StringBuilder result = new StringBuilder();
         result.append("Rental Record for ").append(getName()).append("\n");
 
         for (Rental rental : rentals) {
-            frequentRenterPoints += rental.getFrequentRenterPoints();
 
             // show figures for this rental
             result.append("\t").append(rental.getMovie().getTitle()).append("\t")
                     .append(String.valueOf(rental.getCharge())).append("\n");
-            totalAmount += rental.getCharge();
         }
         // add footer lines
-        result.append("Amount owed is ").append(String.valueOf(totalAmount)).append("\n");
-        result.append("You earned ").append(String.valueOf(frequentRenterPoints)).append(" frequent renter points");
+        result.append("Amount owed is ").append(String.valueOf(getTotalCharge())).append("\n");
+        result.append("You earned ").append(String.valueOf(getFrequentRenterPoints())).append(" frequent renter points");
         return result.toString();
+    }
+
+    private int getFrequentRenterPoints() {
+        return rentals.stream().mapToInt(Rental::getFrequentRenterPoints).sum();
+    }
+
+    private double getTotalCharge() {
+        return rentals.stream().mapToDouble(Rental::getCharge).sum();
     }
 
 }
