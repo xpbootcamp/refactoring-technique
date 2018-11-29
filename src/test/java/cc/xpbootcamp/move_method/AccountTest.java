43ce6bc3e3ebecfb197b4edeccd0dfb48a8a767e
Yuan Shenjian <sjyuan@thoughtworks.com>
2018-11-29 15:17:00 +0800

    MM: Modify test
    Signed-off-by: Yuan Shenjian <sjyuan@thoughtworks.com>

--- src/test/java/cc/xpbootcamp/move_method/AccountTest.java
+++ src/test/java/cc/xpbootcamp/move_method/AccountTest.java
@@ -7,21 +7,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 class AccountTest {
 
     @Test
-    void should_calculate_correct_overdraft_charge_when_premium() {
-        double result = new Account(8, new AccountType(AccountType.TYPE_CODE_PREMIUM)).overdraftCharge();
-        assertThat(result).isEqualTo(10.85);
+    void should_calculate_correct_bank_charge_when_premium() {
+        double result = new Account(8, new AccountType(AccountType.TYPE_CODE_PREMIUM)).bankCharge();
+        assertThat(result).isEqualTo(15.35);
     }
 
 
     @Test
-    void should_calculate_correct_overdraft_charge_when_premium_and_days_below_7() {
-        double result = new Account(6, new AccountType(AccountType.TYPE_CODE_PREMIUM)).overdraftCharge();
-        assertThat(result).isEqualTo(4.5);
+    void should_calculate_correct_bank_charge_when_premium_and_days_below_7() {
+        double result = new Account(6, new AccountType(AccountType.TYPE_CODE_PREMIUM)).bankCharge();
+        assertThat(result).isEqualTo(9.0);
     }
 
     @Test
-    void should_calculate_correct_overdraft_charge_when_non_premium() {
-        double result = new Account(5, new AccountType("non-premium")).overdraftCharge();
-        assertThat(result).isEqualTo(3.75);
+    void should_calculate_correct_bank_charge_when_non_premium() {
+        double result = new Account(5, new AccountType("non-premium")).bankCharge();
+        assertThat(result).isEqualTo(8.25);
     }
 }
