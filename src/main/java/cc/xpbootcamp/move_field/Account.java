4086dc6d23b08d881b0f47b29c1191de42bddb69
Yuan Shenjian <sjyuan@thoughtworks.com>
2018-11-29 15:35:51 +0800

    MF: Move field to AccountType
    Signed-off-by: Yuan Shenjian <sjyuan@thoughtworks.com>

--- src/main/java/cc/xpbootcamp/move_field/Account.java
+++ src/main/java/cc/xpbootcamp/move_field/Account.java
@@ -2,22 +2,13 @@ package cc.xpbootcamp.move_field;
 
 public class Account {
     private AccountType type;
-    private double interestRate;
 
     public Account(double interestRate, AccountType type) {
-        setInterestRate(interestRate);
+        type.setInterestRate(interestRate);
         this.type = type;
     }
 
-    private void setInterestRate(double interestRate) {
-        this.interestRate = interestRate;
-    }
-
     double interest(double amount, int days) {
-        return getInterestRate() * amount * days / 365;
-    }
-
-    private double getInterestRate() {
-        return interestRate;
+        return type.getInterestRate() * amount * days / 365;
     }
 }
--- src/main/java/cc/xpbootcamp/move_field/AccountType.java
+++ src/main/java/cc/xpbootcamp/move_field/AccountType.java
@@ -1,5 +1,13 @@
 package cc.xpbootcamp.move_field;
 
 public class AccountType {
+    private double interestRate;
 
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
 }
