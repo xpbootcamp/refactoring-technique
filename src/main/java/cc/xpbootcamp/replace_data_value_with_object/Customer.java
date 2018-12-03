e7cfe03b6efdff0c8bd54039fa35480c69e24ed7
Yuan Shenjian <sjyuan@thoughtworks.com>
2018-12-03 23:41:14 +0800

    CVTR: Obtain existed customer if created
    Signed-off-by: Yuan Shenjian <sjyuan@thoughtworks.com>

--- src/main/java/cc/xpbootcamp/replace_data_value_with_object/Customer.java
+++ src/main/java/cc/xpbootcamp/replace_data_value_with_object/Customer.java
@@ -1,6 +1,11 @@
 package cc.xpbootcamp.replace_data_value_with_object;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Customer {
+    private static Map<String, Customer> customers = new HashMap<>();
+
     private String name;
 
     private Customer(String name) {
@@ -15,7 +20,12 @@ public class Customer {
         this.name = name;
     }
 
-    public static Customer create(String name) {
-        return new Customer(name);
+    public static Customer obtainCustomer(String name) {
+        if (customers.containsKey(name)){
+            return customers.get(name);
+        }
+        Customer customer = new Customer(name);
+        customers.put(name, customer);
+        return customer;
     }
 }
